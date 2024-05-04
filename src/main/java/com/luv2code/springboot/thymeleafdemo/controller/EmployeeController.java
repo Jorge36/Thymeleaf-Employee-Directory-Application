fa475d49f096b11a8b72ec0789ef44d4f743a8fe
Jorge36 <36677503+Jorge36@users.noreply.github.com>
2024-05-04 20:12:25 +0100

    Update EmployeeController.java

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -100,7 +100,7 @@ public class EmployeeController {
 		// assign result to model
 		model.addAttribute("employees", employees);
 		
-		return "/employees/list-employees";
+		return "employees/list-employees";
 		
 	}
 	
